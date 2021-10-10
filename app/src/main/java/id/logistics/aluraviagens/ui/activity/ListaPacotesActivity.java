edeea62d19e6e4d546f338448bae1b6391338f03
Junior Fernandes <afernandes@id-logistics.com.br>
2021-10-10 16:37:42 -0300

    Introdução ao Constraint Layout

---
+++ app/src/main/java/id/logistics/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,17 @@
+package id.logistics.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.logistics.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+
+}
