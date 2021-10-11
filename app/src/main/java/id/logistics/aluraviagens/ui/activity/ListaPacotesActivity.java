64af33d2d3f1911f46ad094d190d6c0533c9493c
Junior Fernandes <afernandes@id-logistics.com.br>
2021-10-11 11:22:31 -0300

    Implementando a lista personalizada

--- app/src/main/java/id/logistics/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/id/logistics/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,14 @@ package id.logistics.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import id.logistics.aluraviagens.R;
+import id.logistics.aluraviagens.dao.PacoteDAO;
+import id.logistics.aluraviagens.model.Pacote;
+import id.logistics.aluraviagens.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,6 +18,11 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 
 }
